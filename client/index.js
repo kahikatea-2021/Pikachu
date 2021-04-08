8c4d3a6705e6e31692712096a3fc6d44880ec287
Lorena Costa <loraphaela@gmail.com>
2021-04-09 10:45:27 +1200

    fixes

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router } from "react-router-dom";
 
-import App from './App'
+import App from "./App";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
     <Router>
-    <App />
+      <App />
     </Router>,
-    document.getElementById('app')
-  )
-})
+    document.getElementById("app")
+  );
+});
