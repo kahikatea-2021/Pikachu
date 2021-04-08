e4f05cbd54322dc72f631f944fac50ddf366c806
Lorena Costa <loraphaela@gmail.com>
2021-04-09 11:58:42 +1200

    edit history page

--- client/components/History.jsx
+++ client/components/History.jsx
@@ -5,13 +5,15 @@ import { Route } from "react-router-dom";
 function History() {
   return (
     <div>
-      <h1>Battle History</h1>
-      <div className="row">
-        <div className="col">
+      <h1 className="text-center mt-8 text-xl ">Battle History</h1>
+      <div className="flex  justify-between">
+        <div className="mx-32 mt-10 text-lg">
           Winners
-          <p>Name</p> <p>Points</p>
+          <p className="text-sm mt-4">
+            Name <img src="" alt="" />
+          </p>
         </div>
-        <div className="col">Losers</div>
+        <div className="mx-32 mt-10 text-lg">Losers</div>
       </div>
     </div>
   );
