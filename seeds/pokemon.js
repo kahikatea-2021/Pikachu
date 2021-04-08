2a0be7dc5d2a7074c6baf8aa0ceba91c55628dc1
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-04-09 10:10:32 +1200

    pokemon

--- seeds/pokemon.js
+++ seeds/pokemon.js
@@ -8,7 +8,19 @@ exports.seed = function(knex) {
         {id: 1, name: 'Pikachu', cp: 505},
         {id: 2, name: 'Squirtle', cp: 485},
         {id: 3, name: 'Charmander', cp: 470 },
-        {id: 4, name: 'Bulbasaur', cp: 450 }
+        {id: 4, name: 'Bulbasaur', cp: 450 },
+        {id: 5, name: 'Ivysaur', cp: 805 },
+        {id: 6, name: 'Venusaur', cp: 1005 },
+        {id: 7, name: 'Charmeleon', cp: 905 },
+        {id: 8, name: 'Charizard', cp: 1400 },
+        {id: 9, name: 'Wartortle', cp: 805 },
+        {id: 10, name: 'Blastoise', cp: 1000 },
+        {id: 11, name: 'Ekans', cp: 300 },
+        {id: 12, name: 'Raichu', cp: 1200 },
+        {id: 13, name: 'Clefairy', cp: 510 },
+        {id: 14, name: 'Jigglypuff', cp: 500 },
+        {id: 15, name: 'Zubat', cp: 150 },
+        {id: 16, name: 'Meowth', cp: 305 }
       ]);
     });
 };
