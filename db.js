b4d41770ae7eb0c81f3793ac82e6e58892d44f15
Johnny <jvsainsbury@gmail.com>
2021-04-09 13:24:13 +1200

    add functions to db js

--- db.js
+++ db.js
@@ -9,11 +9,45 @@ function randomPokemon() {
 
 function getPokemon(db = database) {
     const id = randomPokemon()
-    return db('pokemon').where('id', id)   
+    return db('pokemon').where('id', id)
 }
 
 function getUsers(db = database) {
     return db('users').select()
 }
 
-module.export = random
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function battleResult(player1, player2, pokemon1, pokemon2, db = connection) {
+    const number = randomNumber(1, 2)
+    let result = {}
+    if (number === 1) {
+        result = {
+            winner_id: player1,
+            winner_pokemon_id: pokemon1,
+            loser_id: player2,
+            loser_pokemon_id: pokemon2
+        }
+    }
+    else {
+        result = {
+            winner_id: player2,
+            winner_pokemon_id: pokemon2,
+            loser_id: player1,
+            loser_pokemon_id: pokemon1
+        }
+    }
+    return db('history')
+        .insert(result)
+        .then(() => { })
+}
+
+module.export = {
+    battleResult,
+    getPokemon,
+    randomPokemon,
+    getUsers
+
+}
